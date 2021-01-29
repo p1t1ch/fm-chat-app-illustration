c5d0d5b69bd1df5befe12a74c6cf186c01743bda
p1t1ch <belobeev.kirill@gmail.com>
2021-01-29 16:32:59 +0300

    build main chat markup

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -11,17 +11,43 @@ interface ChatProps extends React.HTMLProps<HTMLDivElement> {
 function Chat({ avatar, name, status, message, children, className = '', ...props }: ChatProps) {
   return (
     <article
-      className={`w-chat h-chat bg-secondary-white rounded-chat-outer shadow py-chat-y px-chat-x ${className}`}
+      className={`relative w-chat bg-secondary-white rounded-chat-outer shadow py-chat-y px-chat-x ${className}`}
       {...props}
     >
-      <div className="bg-primary-white" />
-      <div className="gradient" />
-      <div className="grid gap-4 justify-items-start rounded-chat-inner bg-primary-violet-lightest p-chat-inner">
-        {children}
-      </div>
-      <div className="">
-        {message ? <div>{message}</div> : <div>Type a message...</div>}
-        <div />
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-top h-top rounded-br-top rounded-bl-top bg-secondary-white" />
+      <div className="rounded-chat-inner bg-primary-violet-lightest p-chat-inner">
+        <div className="grid grid-cols-profile grid-rows-2 gap-x-2 items-center px-4 pb-3 pt-profile gradient rounded-tl-chat-inner rounded-tr-chat-inner rounded-br-profile rounded-bl-profile">
+          <div className="row-span-full relative w-1 h-arrow">
+            <div className="absolute bg-secondary-white rounded-full" />
+            <div className="absolute bg-secondary-white rounded-full" />
+          </div>
+          <img
+            src={avatar}
+            alt={`${name} avatar`}
+            className="row-span-full w-6 h-6 border border-secondary-white rounded-full"
+          />
+          <div className="font-medium text-secondary-white text-profile">{name}</div>
+          <div className="text-primary-violet-pale text-message">{status}</div>
+          <div className="row-span-full col-start-4 grid gap-circle">
+            <div className="circle" />
+            <div className="circle" />
+            <div className="circle" />
+          </div>
+        </div>
+        <div className="px-screen-x pb-screen-b pt-screen-t">
+          <div className="grid gap-4 justify-items-start h-screen overflow-y-auto">{children}</div>
+          <div className="bg-secondary-white rounded-full pl-5 pr-1 mt-4 h-textbox flex items-center">
+            <div className={`text-textbox ${message ? 'text-primary-violet-darkest' : 'text-primary-blue'} flex-grow`}>
+              {message || 'Type a message...'}
+            </div>
+            <div className="bg-primary-violet-darkest w-send h-send grid place-items-center rounded-full ml-4">
+              <div className="relative w-send-arrow h-send-arrow">
+                <div className="absolute bg-secondary-white rounded-full" />
+                <div className="absolute bg-secondary-white rounded-full" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </article>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,14 +32,29 @@ module.exports = {
       fontSize: {
         heading: '2.5rem',
         message: ['.5rem', 1.375],
+        profile: ['.6875rem', 1.18],
+        textbox: ['.5625rem', 1.18],
       },
       width: {
         chat: '15.4375rem',
         bg: '31.875rem',
+        top: '8.0625rem',
+        circle: '.125rem',
+        send: '1.5625rem',
+        'send-arrow': '.375rem',
       },
       height: {
-        chat: '31.5625rem',
         bg: '63.625rem',
+        top: '1.8125rem',
+        arrow: '.625rem',
+        circle: '.125rem',
+        screen: '21.625rem',
+        textbox: '2.125rem',
+        send: '1.5625rem',
+        'send-arrow': '.625rem',
+      },
+      gap: {
+        circle: '.0625rem',
       },
       maxWidth: {
         message: '8rem',
@@ -61,6 +76,8 @@ module.exports = {
         'chat-inner': '1.25rem',
         message: '.625rem',
         'message-small': '.25rem',
+        top: '.875rem',
+        profile: '.375rem',
       },
       padding: {
         'chat-y': '.6875rem',
@@ -68,6 +85,13 @@ module.exports = {
         'chat-inner': '.0625rem',
         message: '.375rem',
         'message-price': '.625rem',
+        profile: '1.875rem',
+        'screen-x': '.375rem',
+        'screen-t': '.6875rem',
+        'screen-b': '.5625rem',
+      },
+      gridTemplateColumns: {
+        profile: 'auto auto 1fr auto',
       },
     },
   },
